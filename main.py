7d05d82496938e5b1d6132f809b7df544cee87e7
finn8892 <159733074+finn8892@users.noreply.github.com>
2024-05-02 12:17:43 +1200

    g

---
+++ main.py
@@ -0,0 +1,16 @@
+
+print("hi")
+#ask the user their name
+name = input("whats your name ")
+
+#welcome to the quiz
+print ("this quiz is a stupid quiz",name)
+
+#question/answers
+input ("are you stupid ")
+print("yes you are")
+
+
+
+#thanks for playing
+print("thanks for playing")
